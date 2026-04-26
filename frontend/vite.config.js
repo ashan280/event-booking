3de1d2c03fbd200c48a0462f94e7ab51ee2115b1
vinuridisara997 <vinuridisara997@example.com>
2026-04-26 19:59:54 +0530

    fix tailwind linux build

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   base: "/event-booking/",
-  plugins: [tailwindcss(), react()],
+  plugins: [react()],
   server: {
     port: 5173
   }
