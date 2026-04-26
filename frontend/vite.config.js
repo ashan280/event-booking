763856163bd65fc5a4c9abb95c2609de86c3d378
vinuridisara997 <vinuridisara997@example.com>
2026-04-26 19:48:53 +0530

    feat: setup GitHub Actions for GitHub Pages deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: "/Event-Booking-Seat-Reservation/",
+  base: "/event-booking/",
   plugins: [tailwindcss(), react()],
   server: {
     port: 5173
